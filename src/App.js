4fd56457e380865c107650fb81aa6ff2862f4b84
jin1208 <lhdin94@gmail.com>
2020-02-21 10:51:50 +0900

    react , react-router-dom 설치 및 구축

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+      return (
+        <Router>
+          <div>
+            <Route exact path="/" component={App1}/>
+            <Route exact path="/App1" component={App1}/>
+            <Route exact path="/App2" component={App2}/>
+            <Route exact path="/App3" component={App3}/>
+            <Route exact path="/App4" component={App4}/>
+            <Route exact path="/App5" component={App5}/>
+            <Route exact path="/App6" component={App6}/>
+          </div>
+        </Router>
+      );
+    }
 }
 
 export default App;
