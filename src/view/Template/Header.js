738f666219ecfe01cf928702425cc0ae8bc62ce4
jin1208 <lhdin94@gmail.com>
2020-03-09 12:48:32 +0900

    header login logout 추가

--- src/view/Template/Header.js
+++ src/view/Template/Header.js
@@ -7,14 +7,36 @@ import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
 import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import Toolbar from '@material-ui/core/Toolbar';
-import Tooltip from '@material-ui/core/Tooltip';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
 import { withStyles } from '@material-ui/core/styles';
 import styles from '../styles/Header';
 
 function Header(props) {
   const { classes, onDrawerToggle } = props;
+  const [auth, setAuth] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = event => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const logout = event => {
+    setAuth(false);
+  };
 
   return (
     <React.Fragment>
@@ -36,21 +58,54 @@ function Header(props) {
             <Grid item lg />
             <Grid item>
               <Link className={classes.link} href="/Doodle/Paperbase" variant="body2">
-                Go to docs
+                Go to Example
               </Link>
             </Grid>
             <Grid item>
-              <Tooltip title="Alerts • No alerts">
-                <IconButton color="inherit">
-                  <NotificationsIcon />
-                </IconButton>
-              </Tooltip>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
+                  label={auth ? 'Login' : 'Logout'}
+                />
+              </FormGroup>
             </Grid>
             <Grid item>
-              <IconButton color="inherit" className={classes.iconButtonAvatar}>
-                {/* <Avatar src="/static/images/avatar/1.jpg" alt="My Avatar" /> */}
+            {auth && (
+              <div>
+              <IconButton color="inherit" className={classes.iconButtonAvatar} 
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              >
                 <Avatar alt="My Avatar" />
               </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={open}
+                    onClose={handleClose}
+                  >
+                <Link className={classes.link} href="/Doodle/Paperbase" variant="body2">
+                  <MenuItem onClick={handleClose} value='menu001'>UIEXAMPLE</MenuItem>
+                </Link>
+                <Link className={classes.link} href="/Doodle/Search" variant="body2">
+                  <MenuItem onClick={handleClose} value='menu002'>고객관리</MenuItem>
+                </Link>
+                <Link className={classes.link} href="/" variant="body2">
+                  <MenuItem onClick={logout} value='menu003'>로그아웃</MenuItem>
+                </Link>
+              </Menu>
+              </div>
+              )}
             </Grid>
           </Grid>
         </Toolbar>
